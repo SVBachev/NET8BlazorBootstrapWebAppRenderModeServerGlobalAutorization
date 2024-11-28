1bbcd9426c5d22c0d2b343f87e19955c48a9bc4d
s.v.bachev <s.v.bachev@gmail.com>
2024-11-28 14:26:52 +0400

    + Ad authorization and get user name

--- NET8.BlazorWebAppRenderModeServerGlobal/Program.cs
+++ NET8.BlazorWebAppRenderModeServerGlobal/Program.cs
@@ -1,14 +1,22 @@
 using NET8.BlazorWebAppRenderModeServerGlobal.Components;
 using Microsoft.AspNetCore.Authentication.Negotiate;
+using NET8.BlazorWebAppRenderModeServerGlobal;
+using Microsoft.AspNetCore.Components.Authorization;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
 builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
    .AddNegotiate();
 
+builder.Services.AddAuthorization(options =>
+{
+    // By default, all incoming requests will be authorized according to the default policy.
+    options.FallbackPolicy = options.DefaultPolicy;
+});
+
 builder.Services.AddBlazorBootstrap(); // Add this line
 
-// Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
